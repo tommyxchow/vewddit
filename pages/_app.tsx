041fc131c0c930f2f16d8e678b35b90a9c83bf54
Tommy Chow <tommyxchow@gmail.com>
2022-10-24 05:37:49 -0400

    Add `react-query`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,20 @@ import '@fontsource/figtree/400.css';
 import '@fontsource/figtree/500.css';
 import '@fontsource/figtree/600.css';
 import '@fontsource/figtree/700.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute='class' disableTransitionOnChange>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider attribute='class' disableTransitionOnChange>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
