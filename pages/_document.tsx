4c29c8d66aecb9646c3ff005cfc57f60f64b36be
Tommy Chow <tommyxchow@gmail.com>
2022-11-01 03:32:18 -0400

    Add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,15 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang='en'>
-      <Head />
+      <Head>
+        <link
+          rel='icon'
+          type='image/svg+xml'
+          href='/assets/images/favicon.svg'
+        />
+        <link rel='icon' type='image/png' href='/assets/images/favicon.png' />
+      </Head>
+
       <body className='bg-black text-neutral-100'>
         <Main />
         <NextScript />
