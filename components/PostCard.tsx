7c2acbbc12cb157c70bc1bb1242cf0a8e84e6cfb
Tommy Chow <tommyxchow@gmail.com>
2023-02-26 18:19:03 -0500

    Add rounded porder to post card

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -10,17 +10,19 @@ export default function PostCard({ post, onClick }: PostCardProps) {
   const postMedia = parseRedditPostMedia(post);
 
   return (
-    <button aria-label={`Open post titled: ${post.title}`} onClick={onClick}>
-      <article className='aspect-video border-2 border-white border-opacity-0 transition hover:border-opacity-100'>
-        <picture>
-          <img
-            loading='lazy'
-            className='h-full w-full object-cover'
-            src={postMedia?.thumbnailUrl}
-            alt={post.title}
-          />
-        </picture>
-      </article>
+    <button
+      className='aspect-video overflow-clip rounded-lg border-2 border-white border-opacity-0 transition hover:border-opacity-100 hover:opacity-50'
+      aria-label={`Open post titled: ${post.title}`}
+      onClick={onClick}
+    >
+      <picture>
+        <img
+          loading='lazy'
+          className='h-full w-full object-cover'
+          src={postMedia?.thumbnailUrl}
+          alt={post.title}
+        />
+      </picture>
     </button>
   );
 }
