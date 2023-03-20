7e7dc25a4015e638b738323ed31baab8a5f7fc76
Tommy Chow <tommyxchow@gmail.com>
2023-03-19 22:48:20 -0400

    Migrate to `pnpm` and upgrade packages

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,9 +4,9 @@ export default function Header() {
   return (
     <header className='px-4 pt-8'>
       <Link href='/'>
-        <a className='flex w-fit hover:underline'>
-          <h1 className='text-3xl font-bold md:text-4xl'>vewddit</h1>
-        </a>
+        <h1 className='flex w-fit text-3xl font-bold hover:underline md:text-4xl'>
+          vewddit
+        </h1>
       </Link>
 
       <div className='flex flex-col justify-between text-sm text-neutral-400 md:flex-row md:text-base'>
