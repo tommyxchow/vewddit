dd6899089f6f82d2cbff00611a920bbf58ba6085
Tommy Chow <tommyxchow@gmail.com>
2023-11-09 22:10:29 -0500

    Add title and author to post cards

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -10,19 +10,42 @@ export default function PostCard({ post, onClick }: PostCardProps) {
   const postMedia = parseRedditPostMedia(post);
 
   return (
-    <button
-      className='aspect-video overflow-clip rounded-lg border-2 border-white border-opacity-0 transition hover:border-opacity-100 hover:opacity-50'
-      aria-label={`Open post titled: ${post.title}`}
-      onClick={onClick}
-    >
-      <picture>
-        <img
-          loading='lazy'
-          className='h-full w-full object-cover'
-          src={postMedia?.thumbnailUrl}
-          alt={post.title}
-        />
-      </picture>
-    </button>
+    <article className='flex flex-col'>
+      <button
+        className='aspect-video overflow-clip rounded-lg border-2 border-white border-opacity-0 transition hover:border-opacity-100 hover:opacity-50'
+        aria-label={`Open post titled: ${post.title}`}
+        onClick={onClick}
+      >
+        <picture>
+          <img
+            loading='lazy'
+            className='h-full w-full object-cover'
+            src={postMedia?.thumbnailUrl}
+            alt={post.title}
+          />
+        </picture>
+      </button>
+
+      <div className='p-2'>
+        <h2 className='font-semibold'>
+          <a
+            className='hover:underline'
+            target='_blank'
+            href={`https://www.reddit.com${post.permalink}`}
+          >
+            {post.title}
+          </a>
+        </h2>
+        <p>
+          <a
+            className='text-neutral-400 hover:underline'
+            target='_blank'
+            href={`https://www.reddit.com/user/${post.author}`}
+          >
+            u/{post.author}
+          </a>
+        </p>
+      </div>
+    </article>
   );
 }
