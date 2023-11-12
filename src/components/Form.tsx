caf146d90969a8c63ae6445367c5ce3c9c13a6bd
Tommy Chow <tommyxchow@gmail.com>
2023-11-12 02:35:59 -0500

    Reduce form gap

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -69,7 +69,7 @@ export default function SearchBar() {
   }
 
   return (
-    <div className='flex flex-col gap-4 p-4'>
+    <div className='flex flex-col gap-2 p-4'>
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
